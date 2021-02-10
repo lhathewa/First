1d833a9729a8b1a95a131a12008b89aa0a978b70
lhathewa <60928829+lhathewa@users.noreply.github.com>
2021-02-09 19:01:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is Your Name?");
+var color = prompt("What Is Your Favorite Color?");
+document.body.querySelector(".message").innerHTML="My name is "+name+". My Favorite Color Is "+color;
